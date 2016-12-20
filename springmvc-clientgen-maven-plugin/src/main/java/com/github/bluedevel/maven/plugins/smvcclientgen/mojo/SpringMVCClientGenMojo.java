25beaadc499697f88fa4a28d23e1de30ed934cb6
robin <engel.robin@gmx.de>
2016-12-20 18:58:13 +0100

    get integration test suite running

---
+++ springmvc-clientgen-maven-plugin/src/main/java/com/github/bluedevel/maven/plugins/smvcclientgen/mojo/SpringMVCClientGenMojo.java
@@ -0,0 +1,39 @@
+package com.github.bluedevel.maven.plugins.smvcclientgen.mojo;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+/**
+ * @author Robin Engel
+ */
+@Mojo(name = "generate-clients")
+public class SpringMVCClientGenMojo extends AbstractMojo {
+
+    @Parameter(property = "project", readonly = true, required = true)
+    private MavenProject project;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        File outputDirectory = new File(project.getBuild().getOutputDirectory());
+        URL outputURL;
+        try {
+            outputURL = outputDirectory.toURI().toURL();
+        } catch (MalformedURLException e) {
+            throw new MojoExecutionException("Failed to find output folder", e);
+        }
+
+        ClassLoader parentClassLoader = this.getClass().getClassLoader();
+        URLClassLoader classLoader = new URLClassLoader(new URL[]{outputURL}, parentClassLoader);
+
+        getLog().info("!!!");
+    }
+
+}
